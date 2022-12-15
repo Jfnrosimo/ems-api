9bac280b73d4f720a63a12ca33a353c9c5ed7155
jrosi <j.rosimo098@gmail.com>
2022-12-15 21:28:19 +0800

    Feat: added backend for deployment

---
+++ src/auth.js
@@ -0,0 +1,34 @@
+import User from "./models/users.js";
+
+export const authenticateWithToken = (request, response, next) => {
+  const authHeader = request.get("Authorization");
+  if (authHeader) {
+    console.log(authHeader,"header")
+    const match = authHeader.match(/^(Token|Bearer) (.+)/i);
+    if (match) {
+      console.log(match,"match")
+      const token =  match[2];
+      console.log(token,"token")
+     User.findOne({ token }).exec()
+        .then((user) => {
+          request.user = user;
+          next();
+        })
+        .catch((err) => {
+          next(err);
+        });
+      return;
+    }
+  }
+
+  next();
+};
+
+export const requireUser = (request, response,next) => {
+  if (!request.user) {
+    response.status(401).json({ error: "You don't have access to this resource" });
+    return;
+  }
+
+  next();
+};
